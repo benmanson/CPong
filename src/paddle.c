dfbdf5abfa644d9ab681b7ee3d4913c49e9bd8c6
benmanson <benjmanson87@gmail.com>
2022-09-29 21:47:37 +0100

    Change paddle starting position based on id; implement reset_paddle

--- src/paddle.c
+++ src/paddle.c
@@ -1,13 +1,25 @@
 #include "paddle.h"
 
-Paddle* get_paddle()
+Paddle* get_paddle(int id)
 {
     Paddle* paddle = (Paddle*) malloc(sizeof(Paddle));
+    paddle->score = 0;
 
-    paddle->x = 0;
-    paddle->y = SCREEN_HEIGHT / 2;
-    paddle->yVel = 0;
+    switch (id)
+    {
+    case 0:
+        paddle->x = 0;
+        break;
 
+    case 1:
+        paddle->x = SCREEN_WIDTH - PADDLE_WIDTH;
+        break;
+
+    default:
+        exit(1);
+    }
+
+    reset_paddle(paddle);
     return paddle;
 }
 
@@ -17,11 +29,22 @@ void delete_paddle(Paddle* paddle)
     paddle = NULL;
 }
 
+void reset_paddle(Paddle* paddle)
+{
+    paddle->y = SCREEN_HEIGHT / 2;
+    paddle->yVel = 0;
+}
+
 void move_paddle(Paddle* paddle)
 {
-    if (paddle->y >= (SCREEN_HEIGHT - PADDLE_HEIGHT) | paddle->y <= 0)
+    if ((paddle->y+paddle->yVel) >= (SCREEN_HEIGHT - PADDLE_HEIGHT) | (paddle->y+paddle->yVel) <= 0)
     {
         paddle->yVel = 0;
     }
     paddle->y += paddle->yVel;
+}
+
+void score(Paddle* paddle)
+{
+    paddle->score += 1;
 }
